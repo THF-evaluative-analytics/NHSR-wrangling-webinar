834d4c57a2fa8df92dc103b259af9fd79422dcb1
Emma Vestesson <emma.vestesson@gmail.com>
2020-08-19 11:46:18 +0100

    Updates

--- Live_coding.R
+++ Live_coding.R
@@ -5,40 +5,24 @@ sitrep <- readRDS(here::here('data', 'sitrep.rds')) # all calls
 sitrep_60sec <- readRDS(here::here('data', 'sitrep_60sec.rds')) # calls answered within 60sec
 
 # reshape the data
-sitrep_long <- sitrep %>% 
-  pivot_longer(cols=-c(NHS_111_area_name, year),
-               names_to='day_month', 
-               values_to='calls'
-  
-)
-View(sitrep_long)
 
 # fix dates
 
-sitrep_long <- sitrep_long %>% 
-  mutate(date=paste(day_month, year), date=dmy(date))
 
 # Calculate total calls by region
 
-sitrep_calls_by_region <- sitrep_long %>% 
-  group_by(NHS_111_area_name) %>%
-  summarise(total_calls=sum(calls))
   
 # Plot  calls over time
 
-sitrep_long %>% 
-  group_by(date) %>% 
-  summarise(total_calls=sum(calls, na.rm = TRUE)) %>% 
-  ggplot(aes(x=date, y=total_calls)) + geom_line()
 
+## calculate cumulative sum by area and reshape back
 
-## reshape back
+## pivot_wider us_income example
 
-
+us_rent_income
 
 # WHO section ----
 data('who')
 View(who)
 
-who %>% 
-  pivot_longer()
+## join data ---
